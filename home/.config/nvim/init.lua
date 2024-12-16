87c895545987b76c09e9562105a73fffdb28c078
Ryan De Haven <the1bbb@gmail.com>
2024-12-16 13:05:35 -0800

    Get rid of annoying auto close tags, add <c-p> to open file search in telecope

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -460,7 +460,7 @@ require("lazy").setup({
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", function()
+			vim.keymap.set("n", "<C-p>", function()
 				builtin.find_files({ hidden = true })
 			end, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
